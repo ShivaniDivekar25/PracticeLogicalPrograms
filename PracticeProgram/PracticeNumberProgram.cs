cfa6f00393951d0a458fc2a6724e8a8a5be03723
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-03-14 11:23:59 +0530

    Modified to Find Odd Or Even Number program

---
+++ PracticeProgram/PracticeNumberProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProgram
+{
+    public class PracticeNumberProgram
+    {
+        public static void FindOddEven()
+        {
+            Console.WriteLine("Enter user input");
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (input % 2 == 0)
+                Console.WriteLine("The given number {0} is even", input);
+            else
+                Console.WriteLine("The given number {0} is odd", input);
+        }
+    }
+}
--- PracticeProgram/Program.cs
+++ PracticeProgram/Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Practice Programs");
+            Console.WriteLine("Please enter an option");
+            Console.WriteLine("1:Find odd or even number");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    PracticeNumberProgram.FindOddEven();
+                    break;
+                default:
+                    Console.WriteLine("Please enter right option");
+                    break;
+            }
         }
     }
 }
